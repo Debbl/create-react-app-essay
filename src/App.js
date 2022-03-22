4abbe091ee7c1236a155f5bd02fca96ca591e076
Debbl <me@aiwan.run>
2022-03-22 10:33:37 +0800

    install tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h2>App</h2>
+    <div className="h-screen flex justify-center items-center">
+      <h2 className="w-20 text-3xl text-red-300 bg-white font-bold flex justify-center items-center hover:text-red-800 hover:bg-black">
+        App
+      </h2>
     </div>
   );
 }
